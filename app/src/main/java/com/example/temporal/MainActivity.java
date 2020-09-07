1e40ea1200536f7fdce2a59b4e25d1b7fc09989b
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-09-07 16:46:26 +0900

    기본 구조 고안

--- app/src/main/java/com/example/temporal/MainActivity.java
+++ app/src/main/java/com/example/temporal/MainActivity.java
@@ -2,14 +2,33 @@ package com.example.temporal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    final int a = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent next = new Intent(this, interfaceMain.class);
+        Button buttonSignin = findViewById(R.id.buttonSignin);
+        Button buttonSignup = findViewById(R.id.buttonSignup);
+
+        buttonSignin.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(next);
+            }
+        });
+
+        buttonSignup.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/temporal/interfaceMain.java
@@ -0,0 +1,52 @@
+package com.example.temporal;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class interfaceMain extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interface_main);
+        TextView buttonBoard = findViewById(R.id.menuBoard);
+        TextView buttonMap = findViewById(R.id.menuMap);
+        TextView buttonInfo = findViewById(R.id.menuInfo);
+
+        buttonBoard.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                communityMain next = new communityMain();
+                fragmentTransaction.replace(R.id.frameMain, next);
+                fragmentTransaction.addToBackStack(null).commit();
+            }
+        });
+
+        buttonMap.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                mapMain next = new mapMain();
+                fragmentTransaction.replace(R.id.frameMain, next);
+                fragmentTransaction.addToBackStack(null).commit();
+            }
+        });
+
+        buttonInfo.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                infoMain next = new infoMain();
+                fragmentTransaction.replace(R.id.frameMain, next);
+                fragmentTransaction.addToBackStack(null).commit();
+            }
+        });
+    }
+}
