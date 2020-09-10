e2cde188a0f86a26691a6dad04c99d80ca3851c1
yjsong67 <yj_song67@naver.com>
2020-09-11 02:00:26 +0900

    회원가입 서버 연동 (test)

---
+++ app/src/main/java/server/UserDataTask.java
@@ -0,0 +1,17 @@
+package server;
+
+import android.os.AsyncTask;
+
+public class UserDataTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String[] sld){
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+
+    }
+
+}
