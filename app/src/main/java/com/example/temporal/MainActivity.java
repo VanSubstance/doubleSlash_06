fc6db319d4ef6167b8cb54741cda8eaec9c50a7c
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2020-09-11 20:05:11 +0900

    카카오톡 연동
    서버 분과 얘기를 해봐야 할거 같습니다 나머지는

--- app/src/main/java/com/example/temporal/MainActivity.java
+++ app/src/main/java/com/example/temporal/MainActivity.java
@@ -1,20 +1,48 @@
 package com.example.temporal;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.kakao.auth.ApiErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.util.OptionalBoolean;
+import com.kakao.util.exception.KakaoException;
+
+import org.json.JSONArray;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SessionCallback sessionCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sessionCallback = new SessionCallback();
+        Session.getCurrentSession().addCallback(sessionCallback);
+        Session.getCurrentSession().checkAndImplicitOpen();
 
         Intent splash = new Intent(this, splashMain.class);
         startActivity(splash);
@@ -43,10 +71,80 @@ public class MainActivity extends AppCompatActivity {
         });
 
         buttonSignup.setOnClickListener(new Button.OnClickListener() {
-            @Override
+           @Override
             public void onClick(View view) {
                 startActivity(intentSignup);
             }
         });
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 세션 콜백 삭제
+        Session.getCurrentSession().removeCallback(sessionCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+    }
+
+    private class SessionCallback implements ISessionCallback {
+        @Override
+        public void onSessionOpened() {
+            UserManagement.getInstance().me(new MeV2ResponseCallback() {
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    int result = errorResult.getErrorCode();
+
+                    if(result == ApiErrorCode.CLIENT_ERROR_CODE) {
+                        Toast.makeText(getApplicationContext(), "네트워크 연결이 불안정합니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(),"로그인 도중 오류가 발생했습니다: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+                @Override
+                public void onSuccess(MeV2Response result) {
+                    /* 수정해야 할 부분 앞으로 어떻게 해 갈지에 대해 알아야지 수정할거 같습니다.
+                    
+                    Intent intent = new Intent(getApplicationContext(), homeMain.class);
+                    intent.putExtra("name", result.getNickname());
+                    intent.putExtra("profile", result.getProfileImagePath());
+                    if(result.getKakaoAccount().hasEmail() == OptionalBoolean.TRUE)
+                        intent.putExtra("email", result.getKakaoAccount().getEmail());
+                    else
+                        intent.putExtra("email", "none");
+                    if(result.getKakaoAccount().hasGender() == OptionalBoolean.TRUE)
+                        intent.putExtra("gender", result.getKakaoAccount().getGender().getValue());
+                    else
+                        intent.putExtra("gender", "none");
+
+
+                    startActivity(intent);
+                    finish();
+
+                     */
+
+                }
+
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                    Toast.makeText(getApplicationContext(),"세션이 닫혔습니다. 다시 시도해 주세요: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Toast.makeText(getApplicationContext(), "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요: ", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
+
