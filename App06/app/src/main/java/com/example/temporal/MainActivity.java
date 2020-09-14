8c6345923c03c60b1d5738628989fbb9e778a4b5
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-09-14 19:01:30 +0900

    로그인 페이지 수정
    - 이메일 / 비밀번호 / 회원가입 버튼

--- App06/app/src/main/java/com/example/temporal/MainActivity.java
+++ App06/app/src/main/java/com/example/temporal/MainActivity.java
@@ -48,32 +48,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(splash);
 
         final Intent intentSignin = new Intent(this, interfaceMain.class);
-        final Intent intentSignup = new Intent(this, signupMain.class);
         Button buttonSignin = findViewById(R.id.buttonSignin);
-        Button buttonSignup = findViewById(R.id.buttonSignup);
 
         buttonSignin.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editEmail = findViewById(R.id.inputEmail);
-                String inputEmail = editEmail.getText().toString();
-                EditText editPw = findViewById(R.id.inputPw);
-                String inputPw = editPw.getText().toString();
-                // 서버 연결
-                // 1. 이메일 / 패스워드 확인
-                if (inputEmail.equals("asd") && inputPw.equals("asd")) {
-                    startActivity(intentSignin);
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "아이디와 비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-        buttonSignup.setOnClickListener(new Button.OnClickListener() {
-           @Override
-            public void onClick(View view) {
-                startActivity(intentSignup);
+                startActivity(intentSignin);
             }
         });
 
