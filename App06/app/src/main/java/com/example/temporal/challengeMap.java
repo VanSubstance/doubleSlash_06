5c80759dc8ace8c5240eacddfb05e02b35a10312
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2020-09-22 17:46:58 +0900

    지도 중간버전 건들지마세용
    스크롤 뷰를 없애는게 나을거 같아서 없앴어용

--- App06/app/src/main/java/com/example/temporal/challengeMap.java
+++ App06/app/src/main/java/com/example/temporal/challengeMap.java
@@ -3,20 +3,46 @@ package com.example.temporal;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+
+import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class challengeMap extends Fragment {
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int PERMISSIONS_REQUEST_ACCESS_CALL_PHONE = 2;
+
+    // 중요...
+    public LocationManager lm;
+
+    // 지도 구현
+    private MapView mapView;
+
+    public double longitude; //경도
+    public double latitude; //위도
+    public double altitude; //고도
+    public float accuracy; //정확도
+    public String provider; //위치제공자
+    public String currentLocation; // 그래서 최종 위치
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.challenge_map, container,false);
+        initView(view);
+        setLocation();
         /**
         MapView mapView = new MapView(inflater.getContext());
         ViewGroup mapViewContainer = (ViewGroup)view.findViewById(R.id.map_view);
@@ -34,4 +60,105 @@ public class challengeMap extends Fragment {
         mapView.zoomOut(true);
          */
         return view;
-    }}
+    }
+
+    private void initView(View view) {
+        mapView= new MapView(getContext());
+        ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+    }
+    private void setLocation() {
+        lm=(LocationManager)getActivity().getSystemService(getContext().LOCATION_SERVICE);
+        Toast.makeText(getContext(),"현재 위치를 찾고 있습니", Toast.LENGTH_SHORT);
+        getlocation();
+    }
+
+    private void getlocation() {
+        try {
+            // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록하기~!!!
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                    1, // 통지사이의 최소 변경거리 (m)
+                     mLocationListener);
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                    1, // 통지사이의 최소 변경거리 (m)
+                   mLocationListener);
+
+            //txtCurrentPositionInfo.setText("위치정보 미수신중");
+            //lm.removeUpdates(mLocationListener);  //  미수신할때는 반드시 자원해체를 해주어야 한다.
+
+        } catch (SecurityException ex) {
+
+        }
+    }
+    private final LocationListener mLocationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            //여기서 위치값이 갱신되면 이벤트가 발생한다.
+            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
+
+            Log.d("test", "onLocationChanged, location:" + location);
+            longitude = location.getLongitude(); //경도
+            latitude = location.getLatitude();   //위도
+            altitude = location.getAltitude();   //고도
+            accuracy = location.getAccuracy();    //정확도
+            provider = location.getProvider();   //위치제공자
+            //Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
+            //Network 위치제공자에 의한 위치변화
+            //Network 위치는 Gps에 비해 정확도가 많이 떨어진다.
+            // 지도를 움직인다
+            setDaumMapCurrentLocation(latitude, longitude);
+
+            lm.removeUpdates( mLocationListener);  //  미수신할때는 반드시 자원해체를 해주어야 한다.
+
+        }
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+    };
+    public void setDaumMapCurrentLocation(double latitude, double longitude) {
+
+        // 중심점 변경
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude), true);
+
+        // 줌 레벨 변경
+        mapView.setZoomLevel(4, true);
+
+        // 중심점 변경 + 줌 레벨 변경
+        //mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(latitude, longitude), 9, true);
+
+        // 줌 인
+        mapView.zoomIn(true);
+
+        // 줌 아웃
+        //mapView.zoomOut(true);
+
+        // 마커 생성
+        setDaumMapCurrentMarker();
+
+    }
+
+    public void setDaumMapCurrentMarker(){
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("현재 위치");
+        marker.setTag(0);
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude));
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        mapView.addPOIItem(marker);
+    }
+
+}
