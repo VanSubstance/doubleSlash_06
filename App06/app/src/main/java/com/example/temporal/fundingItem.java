2b42b541733522d54accbd4ad2fd11db8136cb3e
yujin <aceyu21@gmail.com>
2020-09-28 18:01:16 +0900

    펀딩페이지
    펀딩페이지 레이아웃 수정 완료

--- App06/app/src/main/java/com/example/temporal/fundingItem.java
+++ App06/app/src/main/java/com/example/temporal/fundingItem.java
@@ -6,25 +6,30 @@ import android.app.Fragment;
 
 public class fundingItem{
     String id;
-    String title;
-    String titleClick;
+    String fundingName;
+    String fundingTitle;
+    String fundingSpecific;
     String specific_title;
     String specific_description;
-    String regDate;
-    String closeDate;
-    Integer targetpoint;
-    Integer presentpoint;
-    String description;
     String descriptionClick;
-    String description2;
+    String nowPoint;
+    String seekmin;
+    String seekmax;
+    String point;
+    String restPoint;
 
     public void init(int i) {
-        title = "펀딩 제목" + i;
-        description = "펀딩 설명" + i;
-        titleClick = "펀딩 제목" + i;
+        fundingName = "그린피스" + i;
+        fundingTitle = "펀딩 제목" + i;
+        fundingSpecific = "펀딩 세부내용 문구" + i;
         descriptionClick = "펀딩 설명" + i;
         specific_title = "펀딩 세부내용 제목" + i;
         specific_description = "펀딩 세부내용" + i;
+        nowPoint = "나의 현재 포인트" + i;
+        seekmin = "0_" + i;
+        seekmax = "5000(현재 포인트)_" + i;
+        point = "펀딩 포인트" + i;
+        restPoint = "잔여 포인트" + i;
     }
 
 }
