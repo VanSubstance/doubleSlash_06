9c6fcd5114757c1cd395d51c7f480d8770bf2fd5
yjsong67 <yj_song67@naver.com>
2020-10-13 18:01:47 +0900

    member/close api 추가

--- Server/miniproject/src/main/java/server06/miniproject/mapper/MemberMapper.java
+++ Server/miniproject/src/main/java/server06/miniproject/mapper/MemberMapper.java
@@ -1,6 +1,8 @@
 package server06.miniproject.mapper;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
@@ -17,5 +19,7 @@ public interface MemberMapper {
 	int updateMemberLocation(@Param("memId") int memId, @Param("item") Member member);
 	
 	int updateMemberPoint(@Param("memId") int memId, @Param("item") Member member);
+	
+	List<Map<String, BigDecimal>> selectCloseMember(@Param("memId") int memId);
 	
 }
